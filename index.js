d6109f4fdc1677aac06f2d3b8ac499be3e7ec1bc
Shama <assiddik001@gmail.com>
2022-05-04 20:19:49 +0600

    Create get api by id

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -9,7 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 // Connect DB
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.eehys.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -27,6 +27,13 @@ async function run() {
       const cursor = inventoryCollection.find(query);
       const inventoris = await cursor.toArray();
       res.send(inventoris);
+    });
+    //   Get data by specific id
+    app.get("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const inventory = await inventoryCollection.findOne(query);
+      res.send(inventory);
     });
   } finally {
     // await client.close();
