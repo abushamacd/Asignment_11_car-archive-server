070adef356c815529bcbaf1e41eaffa94c32423c
Shama <assiddik001@gmail.com>
2022-05-04 19:33:07 +0600

    DB conected

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Connect DB
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.eehys.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("DB Connected");
+  client.close();
+});
+
 app.get("/", (req, res) => {
   res.send("Car Archive sever running");
 });
