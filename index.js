1ee0ce0c91bc5af1e51dc97e416765b7ab5ffa95
Shama <assiddik001@gmail.com>
2022-05-04 19:16:57 +0600

    server ready

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Car Archive sever running");
+});
+
+app.listen(port, () => {
+  console.log(`Car Archive on port ${port}`);
+});
